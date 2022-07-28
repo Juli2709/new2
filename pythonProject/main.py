4b27c16de6d2c2ec1ae40932039134011e28b00e
juliamigay <juliamigay@gmail.com>
2022-07-28 21:24:36 +0300

    Initial commit

---
+++ pythonProject/main.py
@@ -0,0 +1,24 @@
+import random
+p = 0
+a = int(input("введите 1 число от 1 до 20: "))
+b = int(input("введите 2 число от 1 до 20: "))
+c = random.randint(1, 20)
+d = random.randint(1, 20)
+print("третье число: ", c)
+print("четвертое число: ", d)
+sum1 = a + b
+sum2 = c + d
+print("сумма введенных чисел: ", sum1)
+print("сумма рандомных чисел: ", sum2)
+
+while p <= 7:
+    p = p + 1
+    continue
+if sum1 > sum2:
+    print("введенные больше")
+else:
+    print("рандомные больше")
+
+
+
+
